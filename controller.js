720d9f76ad6fe5fa472be8796d7447d822360027
DKOLABS <45796588+DKOLABS@users.noreply.github.com>
2023-01-29 13:55:15 -0500

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,16 +1,20 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    var target = ns.args[0];
-    var scriptMem = ns.getScriptRam("base-hack.js", "home");
-    var clients = ["n00dles", "foodnstuff", "sigma-cosmetics", "joesguns", "hong-fang-tea", "harakiri-sushi", "nectar-net"];
+    const target = ns.args[0];
+    const scriptMem = ns.getScriptRam("base-hack.js", "home");
+    const clients = ["n00dles", "foodnstuff", "sigma-cosmetics", "joesguns", "hong-fang-tea", "harakiri-sushi", "nectar-net", "max-hardware", "CSEC", "neo-net", "zer0", "iron-gym"];
+    const servers = ns.getPurchasedServers();
+    const mergedDevices = [...clients, ...servers];
 
-    for (let i = 0; i < clients.length; i++) {
-        var maxMem = ns.getServerMaxRam(clients[i]);
+    for (let i = 0; i < mergedDevices.length; i++) {
+        var maxMem = ns.getServerMaxRam(mergedDevices[i]);
         var threads = Math.floor(maxMem / scriptMem);
 
-        // ns.exec("NUKE.exe", clients[i]);
-        ns.killall(clients[i]);
-        ns.exec("base-hack.js", clients[i], threads, target);
+        ns.brutessh(mergedDevices[i]);
+        ns.nuke(mergedDevices[i]);
+        ns.killall(mergedDevices[i]);
+        ns.scp("base-hack.js", mergedDevices[i])
+        ns.exec("base-hack.js", mergedDevices[i], threads, target);
 
     }
 }
