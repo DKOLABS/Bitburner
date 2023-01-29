c746c5d5d42c977018d4c1b45a9bb8e0dc750b9f
DKOLABS <45796588+DKOLABS@users.noreply.github.com>
2023-01-29 12:13:22 -0500

    Create purchase-server-8gb.js

---
+++ purchase-server-8gb.js
@@ -0,0 +1,14 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    var ram = 8;
+    var i = 0;
+
+    while (i < ns.getPurchasedServerLimit()) {
+        if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
+            var hostname = ns.purchaseServer("pserv-" + i, ram);
+            ns.scp("base-hack.js", hostname);
+            ns.exec("base-hack.js", hostname, 3, "harakiri-sushi");
+            ++i;
+        }
+    }
+}
