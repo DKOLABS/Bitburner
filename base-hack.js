bd3b20884bcdc69bc8ab237ed5b912c204239f19
DKOLABS <45796588+DKOLABS@users.noreply.github.com>
2023-01-29 12:10:43 -0500

    Update base-hack.js

--- base-hack.js
+++ base-hack.js
@@ -1,22 +1,22 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	var target = ns.args[0];
-	var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
-	var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+    var target = ns.args[0];
+    var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+    var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
 
-	if (ns.fileExists("BruteSSH.exe", "home")) {
-		ns.brutessh(target);
-	}
+    if (ns.fileExists("BruteSSH.exe", "home")) {
+        ns.brutessh(target);
+    }
 
-	ns.nuke(target)
+    ns.nuke(target)
 
-	while (true) {
-		if (ns.getServerSecurityLevel(target) > securityThresh) {
-			await ns.weaken(target);
-		} else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-			await ns.grow(target);
-		} else {
-			await ns.hack(target);
-		}
-	}
+    while (true) {
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            await ns.grow(target);
+        } else {
+            await ns.hack(target);
+        }
+    }
 }
